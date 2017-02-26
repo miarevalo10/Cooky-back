eabc11e5cd76735430431c81721507fe41cf90e3
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-02-26 01:51:42 -0500

    receta module started

---
+++ cheezy/routes/Objects/recetaPueba.js
@@ -0,0 +1,10 @@
+{
+	"nickName": "Josega149",
+	"folder":"subidas",
+	"title":"receta de mi abuela: Lasaña mixta",
+	"description":"La lasaña de mi abuela",
+	"Ingredients":[],
+	"pictureGif":"p"
+}
+
+//nickname es llave primaria
