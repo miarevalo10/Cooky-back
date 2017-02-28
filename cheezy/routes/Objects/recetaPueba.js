216f4c980c109fd40a9ddd62ad2a20fd813d8ec5
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-02-28 18:16:31 -0500

    recipe model completed

--- cheezy/routes/Objects/recetaPueba.js
+++ cheezy/routes/Objects/recetaPueba.js
@@ -1,10 +1,13 @@
 {
 	"nickName": "Josega149",
+	"likesTotal":0,
 	"carpetas": [
 					{
 						"nombreCarpeta":"subidas",
 						"recetasDeLaCarpeta":[
 												{
+													"tipo":1,
+													"likes":0,
 													"title":"Jugo de mora de la abuela",
 													"description":"Se hace jugo de mora con el agua y la mora y el azucar.",
 													"pictureGif":"p",
