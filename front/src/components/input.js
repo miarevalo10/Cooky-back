400bbd670580811d8779dbabeeb4d446c782e896
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-03-04 17:54:08 -0500

    Sign Up Modal

--- front/src/components/input.js
+++ front/src/components/input.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 
+"use strict";
+//Where users may fill in info
 class Input extends Component {
 
-  render() {
-    return(
-      <div className="input">
-        <input id={this.props.name} autocomplete="false" required type={this.props.type} placeholder={this.props.placeholder}/>
-        <label for={this.props.name}></label>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="input">
+                <input id={this.props.name} autocomplete="false" required type={this.props.type} placeholder={this.props.placeholder} value={this.props.val}/>
+                <label for={this.props.name}></label>
+            </div>
+        );
+    }
 
 }
 
