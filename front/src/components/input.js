f7b496a9f65336d7c5429fdc43d53874debfb23c
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-03-04 22:52:52 -0500

    Changes in input

--- front/src/components/input.js
+++ front/src/components/input.js
@@ -4,10 +4,25 @@ import React, {Component} from 'react';
 //Where users may fill in info
 class Input extends Component {
 
+  constructor(props){
+    super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.props.onTextInput(e.target.value);
+  }
+
     render() {
         return (
             <div className="input">
-                <input id={this.props.name} autocomplete="false" required type={this.props.type} placeholder={this.props.placeholder} value={this.props.val}/>
+                <input
+                  id={this.props.name}
+                  autoComplete="false"
+                  required type={this.props.type}
+                  placeholder={this.props.placeholder}
+                  value={this.props.val}
+                  onChange={this.handleInputChange}/>
                 <label for={this.props.name}></label>
             </div>
         );
