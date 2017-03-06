9109bf46ba9a4c2f243465aa4f183518d0dead01
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-03-06 15:04:41 -0500

    add recipe

--- front/src/components/recipes/addRecipe.js
+++ front/src/components/recipes/addRecipe.js
@@ -10,7 +10,11 @@ class AddRecipe extends Component{
   constructor(props){
     super(props);
     this.state = {
-      hola: ''
+      title: '',
+      type:-1,
+      description:'',
+      ingredients: [],
+      pictureGif:' '
     }
   }
 
@@ -20,20 +24,15 @@ class AddRecipe extends Component{
       password: this.props.password,
       folder: 'Favoritos',
       recipe: {
-        tipo: '',
-        likes: 0,
-        creadaPor: this.props.username,
-        title: '',
-        description: '',
-        pictureGif: '',
-        Ingredients:[
-          {
-            ingrediente: '',
-            ingrediente: ''
-          }
-        ]
-      }
-    })
+                tipo: this.props.type,
+                likes: 0,
+                creadaPor: this.props.username,
+                title: this.props.title,
+                description: this.props.description,
+                pictureGif: this.props.pictureGif,
+                Ingredients:this.state.ingredients
+              }
+      })
   }
 
   render(){
@@ -44,6 +43,44 @@ class AddRecipe extends Component{
         <h2>Add a recipe</h2>
         <p>Fill in the form in order to add your recipe</p>
         <form>
+          <Well>
+              <Table condensed hover>
+                  <tbody>
+                      <tr>
+                          <td>Cook</td>
+                          <td>{this.props.username}</td>
+                      </tr>
+                      <tr>
+                          <td>Title</td>
+                          <td><Input name="titulo" type="text"  handleInputChange={this.escribeTitle}
+                          placeholder="Agua panela de la abuela" value={this.state.titulo}/></td>
+                      </tr>
+                      <tr>
+                          <td>Ingredients</td>
+                          <td>{this.props.ingredients.map(ingredient =>{
+                            return(
+                              <ul key={ingredient.ingrediente}>
+                                  <Ingredient name={ingredient.ingrediente}/>
+                              </ul>
+                            );
+                          })}</td>
+                      </tr>
+                      <tr>
+                          <td>Instructions</td>
+                          <td><Input name="instructions" type="text"  handleInputChange={this.escribeInstructions}
+                          placeholder="Se parten 3 limones por la mitad..." value={this.state.titulo}/></td>
+                      </tr>
+                      <tr>
+                          <td>Likes</td>
+                          <td>0</td>
+                      </tr>
+                      <tr>
+                        <td colSpan="2"><Button onClick={() => {this.deleteRecipe(this.props.title)}}  bsStyle="danger">Delete</Button></td>
+                      </tr>
+                  </tbody>
+              </Table>
+
+          </Well>
 
         </form>
       </div>
